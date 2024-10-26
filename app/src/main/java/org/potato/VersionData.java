dc9b3c5288aa0e21cb560a5d86ea0f92ac8afb80
Linus Honer <lhon2701@gmail.com>
2024-10-25 19:49:52 -0500

    VersionData

---
+++ app/src/main/java/org/potato/VersionData.java
@@ -0,0 +1,13 @@
+package org.potato;
+
+public final class VersionData {
+    public static final String mainReleaseVersion = "0";
+    public static final String subReleaseVersion = "0";
+    public static final String snapshotReleaseVersion = "1";
+    public static final String VERISON_NAME = mainReleaseVersion + "."
+        + mainReleaseVersion + "."
+        + subReleaseVersion + "."
+        + snapshotReleaseVersion + ".";
+    public static final String VERSION_DATE = "10/25/2024";
+    public static final String VERSION_DETAILS = "";
+}
